6d6835c6c0a6b8be5152cbaff86f8c83ac66894b
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-10-13 21:39:11 +0200

    cuarta ecuacion resuelta

---
+++ ecuaciones/ecuacion4.py
@@ -0,0 +1,15 @@
+from sympy import*
+import sympy
+import math
+import numpy as np
+import matplotlib as plt
+
+t=sympy.Symbol('t')
+y=sympy.Function('y')
+
+f=(3*t)/2+y(t)/2*t
+
+sympy.Eq(y(t).diff(t), f)
+
+solucion=sympy.dsolve(y(t).diff(t)-f)
+sympy.pprint(solucion)
